286776ea21abb2b3aba77386531e5a09808b80d3
z3roco01 <zerocool@e.email>
2026-01-19 09:54:38 -0600

    init commit

---
+++ src/main/java/z3roco01/nbt/Main.java
@@ -0,0 +1,7 @@
+package z3roco01.nbt;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
---
+++ src/main/java/z3roco01/nbt/NBTFile.java
@@ -0,0 +1,9 @@
+package z3roco01.nbt;
+
+import java.io.File;
+
+public class NBTFile extends File {
+    public NBTFile(String pathname) {
+        super(pathname);
+    }
+}
